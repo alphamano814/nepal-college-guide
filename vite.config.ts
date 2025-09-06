3a19e5fde6b568970c46803ce7cb2a49c52d6321
alphamano814 <alphamano814@gmail.com>
2025-09-06 15:46:09 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/collegeguide-nepal/' : '/',
+  base: mode === 'production' ? '/nepal-college-guide/' : '/',
   server: {
     host: "::",
     port: 8080,
